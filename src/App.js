786647cc4e98984a11780573f87453d9f7d73608
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-05-02 16:22:08 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import logo from "./logo.svg";
 import "./App.css";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
-  return <Login />;
+  return <Home />;
 }
 
 export default App;
