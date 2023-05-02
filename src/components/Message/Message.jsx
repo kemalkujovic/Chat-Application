0b78e517de372d33c45fce47d68bf5e0496cadd0
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-05-02 18:12:06 +0200

    add message and style messages

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import classes from "./Message.module.css";
 const Message = () => {
   return (
-    <div className={classes.message}>
+    <div className={`${classes.message} ${classes.owner}`}>
       <div className={classes.messageInfo}>
         <img src="https://howtodrawforkids.com/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2020/08/13-how-to-draw-a-Person.jpg.webp" />
         <span>just now</span>
       </div>
       <div className={classes.messageContent}>
         <p>Hello</p>
-        {/* <img
+        <img
           src="https://howtodrawforkids.com/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2020/08/13-how-to-draw-a-Person.jpg.webp"
           alt=""
-        /> */}
+        />
       </div>
     </div>
   );
