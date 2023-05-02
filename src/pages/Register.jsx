05ddc56b7b570a2b1b8643e3f427ba2bfbeeed5c
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-05-02 16:17:38 +0200

    style register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from "./Register.module.css";
+import Add from "../img/addAvatar.jpg";
 const Register = () => {
   return (
     <div className={classes.formContainer}>
@@ -10,7 +11,11 @@ const Register = () => {
           <input type="text" placeholder="User Name" />
           <input type="email" placeholder="E-Mail" />
           <input type="password" placeholder="password" />
-          <input type="file" />
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Add an avatar</span>
+          </label>
           <button>Sign Up</button>
         </form>
         <p>You do have an account? Login</p>
