a398fcb72977f9eb99a515abd669e49b5441ed6e
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-05-02 20:44:06 +0200

    send user data in firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_ApiKey,
+  apiKey: "AIzaSyDaRmdTZnGuvMt4XAaz4tvUbEETp66R_YQ",
   authDomain: "chat-230ac.firebaseapp.com",
   projectId: "chat-230ac",
   storageBucket: "chat-230ac.appspot.com",
   messagingSenderId: "634831047814",
   appId: "1:634831047814:web:022d91383d4ca36f331507",
 };
-
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage();
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -15,30 +15,37 @@ const Register = () => {
     const file = e.target[3].files[0];
 
     try {
+      //Create user
       const res = await createUserWithEmailAndPassword(auth, email, password);
 
-      const storageRef = ref(storage, displayName);
+      //Create a unique image name
+      const date = new Date().getTime();
+      const storageRef = ref(storage, `${displayName + date}`);
 
-      const uploadTask = await uploadBytesResumable(storageRef, file);
-      uploadTask.on(
-        (error) => {
-          setErr(true);
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+      await uploadBytesResumable(storageRef, file).then(() => {
+        getDownloadURL(storageRef).then(async (downloadURL) => {
+          try {
+            //Update profile
             await updateProfile(res.user, {
               displayName,
               photoURL: downloadURL,
             });
+            //create user on firestore
             await setDoc(doc(db, "users", res.user.uid), {
               uid: res.user.uid,
               displayName,
               email,
               photoURL: downloadURL,
             });
-          });
-        }
-      );
+
+            // //create empty user chats on firestore
+            // await setDoc(doc(db, "userChats", res.user.uid), {});
+          } catch (err) {
+            console.log(err);
+            setErr(true);
+          }
+        });
+      });
     } catch (error) {
       setErr(true);
     }
