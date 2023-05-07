67803bde2cd9040203c4ddfec84e79fa887c6a5d
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-05-07 19:08:56 +0200

    updateEmail && updateDisplayName

--- src/components/SettingsMenu/SettingsMenu.jsx
+++ src/components/SettingsMenu/SettingsMenu.jsx
@@ -1,4 +1,6 @@
-import React, { useContext, useState } from "react";
+import { updateEmail, updateProfile } from "firebase/auth";
+import { auth } from "../../firebase";
+import React, { useContext, useRef, useState } from "react";
 import classes from "./SettingsMenu.module.css";
 import { AuthContext } from "../../contex/AuthContext";
 import EditIcon from "@mui/icons-material/Edit";
@@ -10,6 +12,9 @@ const SettingsMenu = () => {
   const [editName, setEditName] = useState(false);
   const [editEmail, setEditEmail] = useState(false);
 
+  const inputRef = useRef(null);
+  const emailRef = useRef(null);
+
   const editIconStyle = {
     cursor: "pointer",
   };
@@ -20,20 +25,46 @@ const SettingsMenu = () => {
     setEditEmail(!editEmail);
   };
 
+  const updateName = () => {
+    const displayName = inputRef.current.value;
+    updateProfile(auth.currentUser, {
+      displayName: displayName,
+    })
+      .then(() => {
+        console.log("UPDATE");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleUpdateEmail = () => {
+    const email = emailRef.current.value;
+    updateEmail(auth.currentUser, email)
+      .then(() => {
+        console.log("Email update");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className={classes.settingsContainer}>
       <div className={classes.settingsWrapper}>
         <div className={classes.infoSettings}>
           <p>Name:</p>
           {!editName && <p>{currentUser.displayName}</p>}
-          {editName && <input type="text" placeholder="New Name" />}
+          {editName && (
+            <input type="text" placeholder="New Name" ref={inputRef} />
+          )}
         </div>
         {!editName && (
           <EditIcon style={editIconStyle} onClick={editNameHandler} />
         )}
         {editName && (
           <div>
-            <SaveAsIcon style={editIconStyle} />
+            <SaveAsIcon style={editIconStyle} onClick={updateName} />
             <CloseIcon onClick={editNameHandler} />
           </div>
         )}
@@ -42,14 +73,16 @@ const SettingsMenu = () => {
         <div className={classes.infoSettings}>
           <p>Email:</p>
           {!editEmail && <p>{currentUser.email}</p>}
-          {editEmail && <input type="text" placeholder="New E-mail" />}
+          {editEmail && (
+            <input type="text" placeholder="New E-mail" ref={emailRef} />
+          )}
         </div>
         {!editEmail && (
           <EditIcon style={editIconStyle} onClick={editEmailHandler} />
         )}
         {editEmail && (
           <div>
-            <SaveAsIcon style={editIconStyle} />
+            <SaveAsIcon style={editIconStyle} onClick={handleUpdateEmail} />
             <CloseIcon onClick={editEmailHandler} />
           </div>
         )}
