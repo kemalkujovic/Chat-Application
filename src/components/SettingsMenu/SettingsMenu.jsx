f57bf3f5df3024813d741d2c7d1ef7c6a4edb732
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-05-07 18:31:08 +0200

    add settings menu && style

--- src/components/SettingsMenu/SettingsMenu.jsx
+++ src/components/SettingsMenu/SettingsMenu.jsx
@@ -1,24 +1,50 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import classes from "./SettingsMenu.module.css";
 import { AuthContext } from "../../contex/AuthContext";
 import EditIcon from "@mui/icons-material/Edit";
+import SaveAsIcon from "@mui/icons-material/SaveAs";
 const SettingsMenu = () => {
   const { currentUser } = useContext(AuthContext);
+  const [editName, setEditName] = useState(false);
+  const [editEmail, setEditEmail] = useState(false);
+
+  const editIconStyle = {
+    cursor: "pointer",
+  };
+  const editNameHandler = () => {
+    setEditName(!editName);
+  };
+  const editEmailHandler = () => {
+    setEditEmail(!editEmail);
+  };
+
   return (
     <div className={classes.settingsContainer}>
       <div className={classes.settingsWrapper}>
         <div className={classes.infoSettings}>
           <p>Name:</p>
-          <p>{currentUser.displayName}</p>
+          {!editName && <p>{currentUser.displayName}</p>}
+          {editName && <input type="text" placeholder="New Name" />}
         </div>
-        <EditIcon />
+        {!editName && (
+          <EditIcon style={editIconStyle} onClick={editNameHandler} />
+        )}
+        {editName && (
+          <SaveAsIcon style={editIconStyle} onClick={editNameHandler} />
+        )}
       </div>
       <div className={classes.settingsWrapper}>
         <div className={classes.infoSettings}>
           <p>Email:</p>
-          <p>{currentUser.email}</p>
+          {!editEmail && <p>{currentUser.email}</p>}
+          {editEmail && <input type="text" placeholder="New E-mail" />}
         </div>
-        <EditIcon />
+        {!editEmail && (
+          <EditIcon style={editIconStyle} onClick={editEmailHandler} />
+        )}
+        {editEmail && (
+          <SaveAsIcon style={editIconStyle} onClick={editEmailHandler} />
+        )}
       </div>
     </div>
   );
